6ba6d5222205aeb9442d09536b03928af42abf30
PedroDeSanti <phmartinsanti@gmail.com>
2023-08-11 19:52:13 -0300

    ✨ Add HAL ADC
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>

--- inc/hal/hal_adc.hpp
+++ inc/hal/hal_adc.hpp
@@ -1,14 +1,60 @@
-#ifndef __HAL_ADC_H__
-#define __HAL_ADC_H__
+#ifndef __HAL_ADC_HPP__
+#define __HAL_ADC_HPP__
 
 #include "stdint.h"
+#include "adc.h"
 
-void hal_adc_init(void);
+/*****************************************
+ * Class Declaration
+ *****************************************/
 
-void hal_adc_start_dma(uint32_t* buffer, uint32_t buffer_size);
+class HalAdc {
+    public:
+        HalAdc(ADC_HandleTypeDef* adc_handle, uint8_t number_of_channels, uint16_t reading_per_channel);
 
-void hal_adc_stop_dma(void);
+        /**
+         * @brief Start the ADC conversion
+         */
+        void start_dma(void);
 
-void hal_adc_callback(void);
+        /**
+         * @brief Stop the ADC conversion
+         */
+        void stop_dma(void);
 
-#endif // __HAL_ADC_H__
+        /**
+         * @brief Check if the ADC conversion is done
+         *
+         * @return true if the conversion is done
+         * @return false if the conversion is not done
+         */
+        bool is_reading_done(void);
+
+        /**
+         * @brief Set the reading done object
+         */
+        static void set_reading_done(void);
+
+    private:
+        ADC_HandleTypeDef* adc_handle;
+        uint32_t* buffer;
+        uint32_t buffer_size;
+        uint8_t number_of_channels;
+        uint16_t reading_per_channel;
+        static bool reading_done;
+
+        // std::array<uint16_t, number_of_sensors> adc_reading;
+
+        // void clear_adc_reading();
+
+        // void clear_adc_reading();
+
+        // void average_adc_reading();
+};
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
+    (void) (hadc);
+    HalAdc::set_reading_done();
+};
+
+#endif // __HAL_ADC_HPP__
--- src/hal/hal_adc.cpp
+++ src/hal/hal_adc.cpp
@@ -1,17 +1,29 @@
-#include "hal/hal_adc.h"
-#include "adc.h"  // apagar
+#include "hal/hal_adc.hpp"
+#include "adc.h"
 
-void hal_adc_init(void) {
+HalAdc::HalAdc(ADC_HandleTypeDef* adc_handle, uint8_t number_of_channels, uint16_t reading_per_channel) {
     MX_ADC1_Init();
+    this->adc_handle = adc_handle;
+    this->number_of_channels = number_of_channels;
+    this->reading_per_channel = reading_per_channel;
+    this->buffer = buffer;
+    this->buffer_size = buffer_size;
+    HalAdc::reading_done = false;
 }
 
-void hal_adc_start_dma(uint32_t* buffer, uint32_t buffer_size) {
-    HAL_ADC_Start_DMA(&hadc1, buffer, buffer_size);
+void HalAdc::start_dma(void) {
+    HalAdc::reading_done = false;
+    HAL_ADC_Start_DMA(this->adc_handle, this->buffer, this->buffer_size);
 }
 
-void hal_adc_stop_dma(void) {
-    HAL_ADC_Stop_DMA(&hadc1);
+void HalAdc::stop_dma(void) {
+    HAL_ADC_Stop_DMA(this->adc_handle);
 }
 
-void hal_adc_callback(void) {
+bool HalAdc::is_reading_done(void) {
+    return HalAdc::reading_done;
+}
+
+void HalAdc::set_reading_done(void) {
+    HalAdc::reading_done = true;
 }
