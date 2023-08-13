abf9c6e7411a099b18bac47c22da7e7a7e938036
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2023-08-13 19:46:23 -0300

    📝 Add documentation and const to hal gpio

--- inc/hal/hal_gpio.hpp
+++ inc/hal/hal_gpio.hpp
@@ -1,8 +1,8 @@
 #ifndef __HAL_GPIO_HPP__
 #define __HAL_GPIO_HPP__
 
-#include <stdbool.h>
-#include <stdint.h>
+#include <cstdint>
+
 #include "gpio.h"
 
 /*****************************************
@@ -11,17 +11,38 @@
 
 class HalGpio {
     public:
+        /**
+         * @brief Construct a new Hal GPIO object
+         *
+         * @param gpio_port pointer to the GPIO port
+         * @param gpio_pin number of the GPIO pin
+         */
         HalGpio(GPIO_TypeDef* gpio_port, uint16_t gpio_pin);
 
-        bool read(void);
+        /**
+         * @brief Read the GPIO pin
+         *
+         * @return true if the pin is high, false otherwise
+         */
+        bool read(void) const;
 
-        void write(GPIO_PinState pin_state);
+        /**
+         * @brief Write to the GPIO pin
+         *
+         * @param pin_state state of the GPIO pin
+         */
+        void write(GPIO_PinState pin_state) const;
 
     private:
+        /**
+         * @brief Pointer to the GPIO port
+         */
         GPIO_TypeDef* gpio_port;
+
+        /**
+         * @brief Number of the GPIO pin
+         */
         uint16_t gpio_pin;
-        GPIO_PinState pin_state;
-        static bool is_gpio_inicialized;
 };
 
 #endif // __HAL_GPIO_HPP__
--- src/hal/hal_gpio.cpp
+++ src/hal/hal_gpio.cpp
@@ -1,23 +1,16 @@
-
 #include "hal/hal_gpio.hpp"
 
-bool HalGpio::is_gpio_inicialized = false;
-
 HalGpio::HalGpio(GPIO_TypeDef* gpio_port, uint16_t gpio_pin) {
-    if (!HalGpio::is_gpio_inicialized) {
-        HalGpio::is_gpio_inicialized = true;
-        MX_GPIO_Init();
-    }
+    MX_GPIO_Init();
 
     this->gpio_pin = gpio_pin;
     this->gpio_port = gpio_port;
-    this->pin_state = GPIO_PIN_RESET;
 }
 
-bool HalGpio::read(void) {
+bool HalGpio::read(void) const {
     return HAL_GPIO_ReadPin(this->gpio_port, this->gpio_pin);
 }
 
-void HalGpio::write(GPIO_PinState pin_state) {
+void HalGpio::write(GPIO_PinState pin_state) const {
     HAL_GPIO_WritePin(this->gpio_port, this->gpio_pin, pin_state);
 }
