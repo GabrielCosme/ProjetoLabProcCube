5e5e5e5cd851e2e4493a537c0fc10687caeeea2b
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2023-08-13 20:11:28 -0300

    📝 Add documentation to motor

--- inc/proxy/motor.hpp
+++ inc/proxy/motor.hpp
@@ -7,10 +7,19 @@
 
 class Motor {
     public:
+        /**
+         * @brief Construct a new Motor object
+         *
+         * @param htim pointer to the timer handler
+         * @param deadzone minumum command to start the motor
+         */
         Motor(TIM_HandleTypeDef* htim, float deadzone);
 
-        ~Motor() = default;
-
+        /**
+         * @brief Set the speed object
+         *
+         * @param speed speed of the motor
+         */
         void set_speed(uint8_t speed);
 
     private:
