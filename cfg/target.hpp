212f72cbef172cd42a0b4dd9e80ba96179479a64
PedroDeSanti <phmartinsanti@gmail.com>
2023-08-14 08:08:58 -0300

    🔧 ⚡️ Update target cfg

--- cfg/target.hpp
+++ cfg/target.hpp
@@ -4,20 +4,24 @@
 #include <cstdint>
 
 #include "gpio.h"
+#include "tim.h"
+#include "adc.h"
+#include "proxy/button.hpp"
 
-const GPIO_TypeDef* button_gpio_port = GPIOB;
+GPIO_TypeDef* button_gpio_port = GPIOB;
 constexpr uint16_t button_pin = GPIO_PIN_10;
+const button_pull_resistor_t button_pull_resistor = BUTTON_PULL_UP;
 
-const GPIO_TypeDef* led_gpio_port = GPIOB;
+GPIO_TypeDef* led_gpio_port = GPIOB;
 constexpr uint16_t led_pin = GPIO_PIN_13;
 
-const TIM_HandleTypeDef* left_motor_timer_handle = &htim2;
-const TIM_HandleTypeDef* right_motor_timer_handle = &htim1;
+TIM_HandleTypeDef* left_motor_timer_handle = &htim2;
+TIM_HandleTypeDef* right_motor_timer_handle = &htim1;
 
 constexpr uint16_t motor_backward_timer_channel = TIM_CHANNEL_2;
 constexpr uint16_t motor_forward_timer_channel = TIM_CHANNEL_1;
 
-const ADC_HandleTypeDef* line_sensor_adc_handle = &hadc1;
+ADC_HandleTypeDef* line_sensor_adc_handle = &hadc1;
 constexpr uint16_t adc_num_channels = 8;
 constexpr uint16_t adc_readings_per_channel = 50;
 
