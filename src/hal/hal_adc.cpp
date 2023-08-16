42caa98b4b30d46922f885752747ea6103c1feb4
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2023-08-16 14:46:23 -0300

    🐛 Fix adc readings

--- src/hal/hal_adc.cpp
+++ src/hal/hal_adc.cpp
@@ -21,9 +21,10 @@ HalAdc<number_of_channels>::HalAdc(const AdcConfig& adc_config) {
     adc_set_right_aligned(adc_config.adc_number);
     adc_enable_scan_mode(adc_config.adc_number);
     adc_set_single_conversion_mode(adc_config.adc_number);
-    adc_enable_discontinuous_mode_regular(adc_config.adc_number);
-    adc_disable_external_trigger_regular(adc_config.adc_number);
+    adc_disable_discontinuous_mode_regular(adc_config.adc_number);
+    adc_eoc_after_each(adc_config.adc_number);
 
+    adc_disable_external_trigger_regular(adc_config.adc_number);
     adc_set_regular_sequence(adc_config.adc_number, number_of_channels, adc_config.channels);
     adc_set_sample_time_on_all_channels(adc_config.adc_number, adc_config.sample_time);
 
@@ -32,13 +33,14 @@ HalAdc<number_of_channels>::HalAdc(const AdcConfig& adc_config) {
 
 template <uint8_t number_of_channels>
 void HalAdc<number_of_channels>::update_reading(void) {
-    for (uint8_t i = 0; i < number_of_channels; i++) {
-        adc_start_conversion_regular(this->adc_number);
+    adc_start_conversion_regular(this->adc_number);
 
-        while (not  adc_eoc(this->adc_number)) {
+    for (uint8_t i = 0; i < number_of_channels; i++) {
+        while (not adc_eoc(this->adc_number)) {
             continue;
         }
 
+        adc_clear_flag(this->adc_number, ADC_SR_EOC);
         this->adc_reading[i] = adc_read_regular(this->adc_number);
     }
 }
